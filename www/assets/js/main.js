af1aed62823c55a8a1cd482153e5a4054098d1a7
Nick Adams <nick@nick-adams.com>
2015-05-28 22:12:11 -0400

    Moved Jekyll config location to fix GH Pages issue

---
+++ www/assets/js/main.js
@@ -0,0 +1,20 @@
+// Rotate Header Text
+$(function(){
+    $(".Hero-Question--rotated").typed({
+      strings: [
+        "focused on the most important problems?",
+        "had all past research at their fingertips?",
+        "did more effective user interviews?",
+        "always cataloged and shared their data?",
+        // "could see the whole picture?", 
+        // "made smarter decisions?",
+        // "built on past learning?",
+        "better understood your user's behavior?",
+        "made more informed design decisions?"
+        ],
+      typeSpeed: 0,
+      backDelay: 5000,
+      loop: false,
+      showCursor: false,
+    });
+});
